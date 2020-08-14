4fd62aac3ea4aa466356912a1410b12fc4e3ffba
Hugo Felipe Torrico Marquez <htorrico@truenorthcorporation.com>
2020-08-13 22:28:29 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio04/Ejercicio04/Models/HomeMenuItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio04.Models
+{
+    public enum MenuItemType
+    {
+        Browse,
+        About,
+        Example,
+        ListViewDemo
+    }
+    public class HomeMenuItem
+    {
+        public MenuItemType Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ Ejercicio04/Ejercicio04/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Ejercicio04.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
